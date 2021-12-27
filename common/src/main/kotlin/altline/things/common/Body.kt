a61666697d6166986bf1e5e583b311e5e380cce8
Dominik Zivko <domy.zivko@gmail.com>
2021-12-27 15:29:25 +0100

    Add stain support to Body

--- common/src/main/kotlin/altline/things/common/Body.kt
+++ common/src/main/kotlin/altline/things/common/Body.kt
@@ -1,8 +1,13 @@
 package altline.things.common
 
 import altline.things.measure.Volume
+import altline.things.substances.MutableSubstance
+import altline.things.substances.Substance
 import io.nacular.measured.units.Measure
 
 interface Body {
     val volume: Measure<Volume>
+
+    fun stain(substance: Substance)
+    fun clearStain(amount: Measure<Volume>): MutableSubstance
 }
--- fabric/src/main/kotlin/altline/things/fabric/Fabric.kt
+++ fabric/src/main/kotlin/altline/things/fabric/Fabric.kt
@@ -4,16 +4,26 @@ import altline.things.common.Body
 import altline.things.substances.Soakable
 import altline.things.measure.*
 import altline.things.substances.MutableSubstance
+import altline.things.substances.Substance
 import io.nacular.measured.units.Measure
 
 abstract class Fabric(
     override val volume: Measure<Volume>
 ) : Body, Soakable {
 
+    private val stain = MutableSubstance()
     private val soakedSubstance = MutableSubstance()
 
     val totalVolume: Measure<Volume>
-        get() = volume + soakedSubstance.totalAmount
+        get() = volume + stain.totalAmount + soakedSubstance.totalAmount
+
+    override fun stain(substance: Substance) {
+        stain.add(substance)
+    }
+
+    override fun clearStain(amount: Measure<Volume>): MutableSubstance {
+        return stain.separate(amount)
+    }
 
     override fun soak(substance: MutableSubstance) {
         val soakableAmount = volume - soakedSubstance.totalAmount
