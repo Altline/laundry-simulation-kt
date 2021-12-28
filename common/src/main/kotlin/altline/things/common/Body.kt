7ded3e2acbd7ef1f97d69e0aef03cf75d955fda3
Dominik Zivko <domy.zivko@gmail.com>
2021-12-28 18:07:39 +0100

    Add convenience for getting the volume sum of a collection of bodies

--- common/src/main/kotlin/altline/things/common/Body.kt
+++ common/src/main/kotlin/altline/things/common/Body.kt
@@ -1,6 +1,7 @@
 package altline.things.common
 
 import altline.things.measure.Volume
+import altline.things.measure.sumOf
 import altline.things.substances.MutableSubstance
 import altline.things.substances.Substance
 import io.nacular.measured.units.Measure
@@ -11,3 +12,6 @@ interface Body {
     fun stain(substance: Substance)
     fun clearStain(amount: Measure<Volume>): MutableSubstance
 }
+
+val <T : Body> Collection<T>.volume: Measure<Volume>
+    get() = sumOf { it.volume }
