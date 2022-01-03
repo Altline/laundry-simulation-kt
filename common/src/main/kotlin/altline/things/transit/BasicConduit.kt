f88bec3c8de67741ee6b6507fd6cbd97d642be10
Dominik Zivko <domy.zivko@gmail.com>
2022-01-03 16:35:04 +0100

    Redesign flow dynamics system to allow for parallel connections

--- common/src/main/kotlin/altline/things/transit/BasicConduit.kt
+++ common/src/main/kotlin/altline/things/transit/BasicConduit.kt
@@ -3,59 +3,57 @@ package altline.things.transit
 import io.nacular.measured.units.*
 
 open class BasicConduit<QuantityType : Units, FlowableType : Flowable<QuantityType>>(
-    override val maxFlowRate: Measure<UnitsRatio<QuantityType, Time>>
+    override val maxFlowRate: Measure<UnitsRatio<QuantityType, Time>>,
+    final override val inputCount: Int = 1,
+    final override val outputCount: Int = 1
 ) : Conduit<QuantityType, FlowableType> {
+
+    init {
+        require(inputCount > 0)
+        require(outputCount > 0)
+    }
 
     private val seenIDs = mutableListOf<Long>()
 
-    override var flowSource: FlowSource<QuantityType, FlowableType>? = null
-        protected set
+    override val inputs = Array(inputCount) { FlowDrain.Port(this) }
+    override val outputs = Array(outputCount) { FlowSource.Port(this) }
 
-    override var flowDrain: FlowDrain<QuantityType, FlowableType>? = null
-        protected set
+    private val connectedSources: List<FlowSource<QuantityType, FlowableType>>
+        get() = inputs.mapNotNull { port -> port.connectedPort?.owner }
 
-    override fun connectFlowSource(source: FlowSource<QuantityType, FlowableType>) {
-        if (this.flowSource == source) return
-        if (this.flowSource != null) disconnectFlowSource()
-        this.flowSource = source
-        source.connectFlowDrain(this)
-    }
+    private val connectedDrains: List<FlowDrain<QuantityType, FlowableType>>
+        get() = outputs.mapNotNull { port -> port.connectedPort?.owner }
 
-    override fun disconnectFlowSource() {
-        this.flowSource?.let {
-            this.flowSource = null
-            it.disconnectFlowDrain()
-        }
-    }
+    fun getSource(index: Int) = inputs[index].connectedPort?.owner
+    fun getDrain(index: Int) = outputs[index].connectedPort?.owner
 
-    override fun connectFlowDrain(drain: FlowDrain<QuantityType, FlowableType>) {
-        if (this.flowDrain == drain) return
-        if (this.flowDrain != null) disconnectFlowDrain()
-        this.flowDrain = drain
-        drain.connectFlowSource(this)
-    }
-
-    override fun disconnectFlowDrain() {
-        this.flowDrain?.let {
-            this.flowDrain = null
-            it.disconnectFlowSource()
-        }
-    }
-
+    @Suppress("UNCHECKED_CAST")
     override fun pushFlow(flowable: FlowableType, timeFrame: Measure<Time>, flowId: Long): Measure<QuantityType> {
-        return flowDrain?.let {
-            if (checkId(flowId)) {
-                it.pushFlow(flowable, timeFrame, flowId)
-            } else null
-        } ?: (0.0 * flowable.amount)
+        var leftoverAmount = (0.0 * flowable.amount)
+        if (checkId(flowId)) {
+            val connectedDrains = connectedDrains
+            val splitAmount = flowable.amount / connectedDrains.size
+            connectedDrains.forEach {
+                val chunk = flowable.extract(splitAmount) as FlowableType
+                leftoverAmount += it.pushFlow(chunk, timeFrame, flowId)
+            }
+        }
+        return leftoverAmount
     }
 
     override fun pullFlow(amount: Measure<QuantityType>, timeFrame: Measure<Time>, flowId: Long): FlowableType? {
-        return flowSource?.let {
-            if (checkId(flowId)) {
-                it.pullFlow(amount, timeFrame, flowId)
-            } else null
+        var pulled: FlowableType? = null
+        if (checkId(flowId)) {
+            val connectedSources = connectedSources
+            val splitAmount = amount / connectedSources.size
+            connectedSources.forEach {
+                it.pullFlow(splitAmount, timeFrame, flowId)?.let { chunk ->
+                    if (pulled == null) pulled = chunk
+                    else pulled!!.add(chunk)
+                }
+            }
         }
+        return pulled
     }
 
     protected fun checkId(flowId: Long): Boolean {
--- common/src/main/kotlin/altline/things/transit/Flow.kt
+++ common/src/main/kotlin/altline/things/transit/Flow.kt
@@ -1,32 +1,84 @@
 package altline.things.transit
 
-import io.nacular.measured.units.Measure
-import io.nacular.measured.units.Time
-import io.nacular.measured.units.Units
-import io.nacular.measured.units.UnitsRatio
+import io.nacular.measured.units.*
 
 interface Flowable<QuantityType : Units> {
     val amount: Measure<QuantityType>
+
+    fun add(other: Flowable<QuantityType>)
+    fun extract(amount: Measure<QuantityType>): Flowable<QuantityType>
 }
 
 interface FlowSource<QuantityType : Units, FlowableType : Flowable<QuantityType>> {
     val maxFlowRate: Measure<UnitsRatio<QuantityType, Time>>
-    val flowDrain: FlowDrain<QuantityType, FlowableType>?
+    val outputCount: Int
 
-    fun connectFlowDrain(drain: FlowDrain<QuantityType, FlowableType>)
-    fun disconnectFlowDrain()
+    val outputs: Array<Port<QuantityType, FlowableType>>
+
+    fun disconnectOutputs() {
+        outputs.forEach { it.disconnect() }
+    }
 
     fun pullFlow(amount: Measure<QuantityType>, timeFrame: Measure<Time>, flowId: Long): FlowableType?
+
+    class Port<QuantityType : Units, FlowableType : Flowable<QuantityType>>(
+        val owner: FlowSource<QuantityType, FlowableType>
+    ) {
+        var connectedPort: FlowDrain.Port<QuantityType, FlowableType>? = null
+            private set
+
+        val isConnected get() = connectedPort != null
+
+        infix fun connectTo(receiverPort: FlowDrain.Port<QuantityType, FlowableType>) {
+            if (connectedPort == receiverPort) return
+            if (connectedPort != null) disconnect()
+            connectedPort = receiverPort
+            receiverPort connectTo this
+        }
+
+        fun disconnect() {
+            connectedPort?.let {
+                it.disconnect()
+                connectedPort = null
+            }
+        }
+    }
 }
 
 interface FlowDrain<QuantityType : Units, FlowableType : Flowable<QuantityType>> {
     val maxFlowRate: Measure<UnitsRatio<QuantityType, Time>>
-    val flowSource: FlowSource<QuantityType, FlowableType>?
+    val inputCount: Int
 
-    fun connectFlowSource(source: FlowSource<QuantityType, FlowableType>)
-    fun disconnectFlowSource()
+    val inputs: Array<Port<QuantityType, FlowableType>>
+
+    fun disconnectInputs() {
+        inputs.forEach { it.disconnect() }
+    }
 
     fun pushFlow(flowable: FlowableType, timeFrame: Measure<Time>, flowId: Long): Measure<QuantityType>
+
+    class Port<QuantityType : Units, FlowableType : Flowable<QuantityType>>(
+        val owner: FlowDrain<QuantityType, FlowableType>
+    ) {
+        var connectedPort: FlowSource.Port<QuantityType, FlowableType>? = null
+            private set
+
+        val isConnected get() = connectedPort != null
+
+        infix fun connectTo(senderPort: FlowSource.Port<QuantityType, FlowableType>) {
+            if (connectedPort == senderPort) return
+            if (connectedPort != null) disconnect()
+            connectedPort = senderPort
+            senderPort connectTo this
+        }
+
+        fun disconnect() {
+            connectedPort?.let {
+                it.disconnect()
+                connectedPort = null
+            }
+        }
+    }
 }
 
 interface Conduit<QuantityType : Units, FlowableType : Flowable<QuantityType>>
