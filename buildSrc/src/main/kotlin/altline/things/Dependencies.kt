a25ad8171d3b20e1850882f8783ed63e746cdf99
Dominik Zivko <domy.zivko@gmail.com>
2022-01-06 16:51:32 +0100

    Update Kotlin and dependencies

--- buildSrc/src/main/kotlin/altline/things/Dependencies.kt
+++ buildSrc/src/main/kotlin/altline/things/Dependencies.kt
@@ -1,15 +1,15 @@
 @file:Suppress("MemberVisibilityCanBePrivate")
 
 object Kotlin {
-    const val version = "1.5.32"
+    const val version = "1.6.10"
 }
 
 object KotlinxCoroutines {
-    const val version = "1.5.2"
+    const val version = "1.6.0"
     const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
 }
 
 object Measured {
-    const val version = "0.3.0"
+    const val version = "0.3.1"
     const val measured = "io.nacular.measured:measured:$version"
 }
