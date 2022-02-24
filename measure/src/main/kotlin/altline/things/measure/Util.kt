a199f8d074d9dbd3693308b907ab22749e121e22
Dominik Živko <domy.zivko@gmail.com>
2022-02-24 16:21:53 +0100

    Enable measures with value of zero to be created generically

--- measure/src/main/kotlin/altline/things/measure/Util.kt
+++ measure/src/main/kotlin/altline/things/measure/Util.kt
@@ -1,12 +1,29 @@
 package altline.things.measure
 
+import altline.things.measure.Energy.Companion.joules
 import altline.things.measure.Frequency.Companion.hertz
+import altline.things.measure.Power.Companion.watts
+import altline.things.measure.Spin.Companion.rpm
+import altline.things.measure.Volume.Companion.liters
 import altline.things.util.closeEquals
 import altline.things.util.isNegligible
 import altline.things.util.isNotNegligible
 import io.nacular.measured.units.*
 import io.nacular.measured.units.Time.Companion.milliseconds
 import io.nacular.measured.units.Time.Companion.seconds
+
+val SUnits: Units? = null
+inline fun <reified T : Units> Units?.zero(): Measure<T> {
+    return when (T::class) {
+        Energy::class -> 0.0 * (joules as T)
+        Power::class -> 0.0 * (watts as T)
+        Frequency::class -> 0.0 * (hertz as T)
+        Time::class -> 0.0 * (seconds as T)
+        Spin::class -> 0.0 * (rpm as T)
+        Volume::class -> 0.0 * (liters as T)
+        else -> throw RuntimeException()
+    }
+}
 
 fun Measure<Frequency>.toPeriod() = (1 / (this `in` hertz)) * seconds
 fun Measure<Time>.toFrequency() = (1 / (this `in` seconds)) * hertz
@@ -17,7 +34,7 @@ suspend fun delay(time: Measure<Time>) = kotlinx.coroutines.delay((time `in` mil
 suspend fun repeatPeriodically(frequency: Measure<Frequency>, times: Int = -1, action: (Long) -> Unit) =
     repeatPeriodically(frequency.toPeriod(), times, action)
 
-suspend fun repeatPeriodically(period: Measure<Time>, times: Int = -1, action: (Long) -> Unit) {
+suspend fun repeatPeriodically(period: Measure<Time>, times: Int = -1, action: suspend (Long) -> Unit) {
     if (times < 0) {
         var count = 0L
         while (true) {
@@ -36,13 +53,13 @@ fun <T : Units> Measure<T>.isNegligible(threshold: Double = 0.00001) = amount.is
 fun <T : Units> Measure<T>.isNotNegligible(threshold: Double = 0.00001) = amount.isNotNegligible(threshold)
 fun <T : Units> Measure<T>.closeEquals(other: Measure<T>, threshold: Double = 0.00001): Boolean {
     val resultUnit = minOf(units, other.units)
-    val a = this  `in` resultUnit
+    val a = this `in` resultUnit
     val b = other `in` resultUnit
     return a.closeEquals(b, threshold)
 }
 
-fun <T, U : Units> Collection<T>.sumOf(selector: (T) -> Measure<U>): Measure<U> {
-    lateinit var sum: Measure<U> // not sure if lateinit will work here
+inline fun <T, reified U : Units> Collection<T>.sumOf(selector: (T) -> Measure<U>): Measure<U> {
+    var sum: Measure<U> = SUnits.zero()
     for (element in this) {
         sum += selector(element)
     }
