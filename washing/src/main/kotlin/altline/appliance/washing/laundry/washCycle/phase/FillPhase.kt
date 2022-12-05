f58dc4d02f7c70808db81a59dfeacf41671d1e5f
Dominik Živko <domy.zivko@gmail.com>
2022-12-05 21:04:26 +0100

    Implement SoftenerFillPhase, extract commonalities with DetergentFillPhase

---
+++ washing/src/main/kotlin/altline/appliance/washing/laundry/washCycle/phase/FillPhase.kt
@@ -0,0 +1,13 @@
+package altline.appliance.washing.laundry.washCycle.phase
+
+import altline.appliance.measure.Volume
+import altline.appliance.washing.laundry.standardIntakeFlowRate
+import io.nacular.measured.units.*
+
+abstract class FillPhase(
+    val fillToAmount: Measure<Volume>
+) : CyclePhase {
+
+    override val duration: Measure<Time>
+        get() = fillToAmount / standardIntakeFlowRate
+}
--- washing/src/main/kotlin/altline/appliance/washing/laundry/washCycle/phase/SoftenerFillPhase.kt
+++ washing/src/main/kotlin/altline/appliance/washing/laundry/washCycle/phase/SoftenerFillPhase.kt
@@ -4,9 +4,9 @@ import altline.appliance.measure.Volume
 import altline.appliance.washing.laundry.StandardLaundryWasherBase
 import io.nacular.measured.units.*
 
-class DetergentFillPhase(fillToAmount: Measure<Volume>) : FillPhase(fillToAmount) {
+class SoftenerFillPhase(fillToAmount: Measure<Volume>) : FillPhase(fillToAmount) {
 
     override suspend fun execute(washer: StandardLaundryWasherBase) {
-        washer.fillThroughDetergent(fillToAmount)
+        washer.fillThroughSoftener(fillToAmount)
     }
 }
