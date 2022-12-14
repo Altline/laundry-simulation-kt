e3e0ce621baefaf9681481296fd50ced7a53016a
Dominik Živko <domy.zivko@gmail.com>
2022-12-14 20:14:08 +0100

    Fix CoroutineManager cancelling scope instead of a launched job

--- util/src/main/kotlin/altline/appliance/util/CoroutineManager.kt
+++ util/src/main/kotlin/altline/appliance/util/CoroutineManager.kt
@@ -1,13 +1,15 @@
 package altline.appliance.util
 
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.cancel
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 class CoroutineManager(
-    val scope: CoroutineScope,
+    private val scope: CoroutineScope,
     private val coroutineBlock: suspend () -> Unit
 ) {
+    private lateinit var job: Job
+
     private var _active = false
     var active: Boolean
         get() = _active
@@ -18,15 +20,15 @@ class CoroutineManager(
 
     private fun launch() {
         if (!_active) {
+            job = scope.launch { coroutineBlock() }
             _active = true
-            scope.launch { coroutineBlock() }
         }
     }
 
     private fun cancel() {
         if (_active) {
             _active = false
-            scope.cancel()
+            job.cancel()
         }
     }
 }
