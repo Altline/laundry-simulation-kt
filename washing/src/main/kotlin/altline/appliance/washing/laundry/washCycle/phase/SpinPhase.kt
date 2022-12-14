7d5638fdd8ee457926b8c4155b11e1db70b5442d
Dominik Živko <domy.zivko@gmail.com>
2022-12-14 16:13:40 +0100

    Skip centrifuge if speed param is 0

--- washing/src/main/kotlin/altline/appliance/washing/laundry/washCycle/phase/SpinPhase.kt
+++ washing/src/main/kotlin/altline/appliance/washing/laundry/washCycle/phase/SpinPhase.kt
@@ -1,17 +1,19 @@
 package altline.appliance.washing.laundry.washCycle.phase
 
+import altline.appliance.measure.Spin.Companion.rpm
 import altline.appliance.washing.laundry.StandardLaundryWasherBase
 import altline.appliance.washing.laundry.washCycle.CentrifugeParams
 import io.nacular.measured.units.*
+import io.nacular.measured.units.Time.Companion.seconds
 
 class SpinPhase(
     val params: CentrifugeParams
 ) : CyclePhaseBase() {
 
     override val duration: Measure<Time>
-        get() = params.duration
+        get() = if (params.duration != 0 * rpm) params.duration else 0 * seconds
 
     override suspend fun doExecute(washer: StandardLaundryWasherBase) {
-        washer.centrifuge(params)
+        if (params.duration != 0 * rpm) washer.centrifuge(params)
     }
 }
