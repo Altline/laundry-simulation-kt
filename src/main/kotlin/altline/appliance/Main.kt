4103c3a0db37d2aa091a2e9e81b63e3e9390757f
Dominik Zivko <domy.zivko@gmail.com>
2022-12-26 21:30:55 +0100

    Set default window size for startup

--- src/main/kotlin/altline/appliance/Main.kt
+++ src/main/kotlin/altline/appliance/Main.kt
@@ -4,7 +4,10 @@ import altline.appliance.di.coreModule
 import altline.appliance.ui.MainScreen
 import altline.appliance.ui.MainViewModel
 import altline.appliance.ui.theme.AppTheme
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import org.koin.core.context.GlobalContext
 import org.koin.core.context.startKoin
@@ -14,7 +17,12 @@ fun main() = application {
     startKoin {
         modules(coreModule)
     }
-    Window(onCloseRequest = ::exitApplication) {
+    Window(
+        onCloseRequest = ::exitApplication,
+        state = WindowState(
+            size = DpSize(1200.dp, 800.dp)
+        )
+    ) {
         window.minimumSize = Dimension(700, 500)
         AppTheme {
             val viewModel = GlobalContext.get().get<MainViewModel>()
